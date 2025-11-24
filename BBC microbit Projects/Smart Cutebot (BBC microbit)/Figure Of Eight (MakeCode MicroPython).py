5b588c30b97ec84c10f9f1fc83912a828e97a60f
Lee Gallagher <lee.gallagher.writing@outlook.com>
2025-11-24 16:55:02 +0000

    Added my folder setup and first couple of files
    Added my folder alongside a couple of small MicroPython files that I made whilst getting started with the BBC Micro:bit's "Smart Cutebot" robot kit, including a "figure of eight" driving pattern and a line tracker using the robot's onboard sensors.

---
+++ BBC microbit Projects/Smart Cutebot (BBC microbit)/Figure Of Eight (MakeCode MicroPython).py
@@ -0,0 +1,28 @@
+# Smart Cutebot (BBC micro:bit) - Figure Of Eight (MakeCode MicroPython)
+
+# This program allows the Smart Cutebot to drive in a simple "figure of eight" pattern
+# I wouldn't say its hugely precise, but I have had to experiment a little and slow mines
+# down considerably as at 100% it is very fast
+# Best flashed onto the BBC micro:bit from the Microsoft MakeCode website - https://makecode.microbit.org/
+
+# On Start - LED Pattern (a big smily face, of course :D)
+
+basic.show_leds("""
+    # # . # #
+    . . . . .
+    # # # # #
+    # . . . #
+    . # # # .
+    """)
+
+# Forever Loop
+
+def on_forever():
+    cuteBot.move_time(cuteBot.Direction.FORWARD, 25, 0.2) # moves forward at 20% speed for 0.2 seconds
+    cuteBot.motors(25, 10) # (left wheel speed, right wheel speed) (25%, 10%) - turns right
+    basic.pause(1500) # continues the turn for 1.5 seconds
+    cuteBot.move_time(cuteBot.Direction.FORWARD, 25, 0.4)
+    cuteBot.motors(10, 25) # (left wheel speed, right wheel speed) (10%, 25%) - turns left
+    basic.pause(1500)
+    cuteBot.move_time(cuteBot.Direction.FORWARD, 25, 0.2)
+basic.forever(on_forever) # runs the forever loop
---
+++ BBC microbit Projects/Smart Cutebot (BBC microbit)/Line Tracking (MakeCode MicroPython).py
@@ -0,0 +1,29 @@
+# BBC micro:bit Smart Cutebot - Line Tracking (MakeCode MicroPython)
+
+# A program that allows the Smart Cutebot to follow a line such as the one
+# provided folded up in the kit
+# Once again, I had to slow it down a bit, not just for my floor this time but for
+# the tracking line provided in the kit - too small to go around it too quickly
+# Once again, the best option for flashing this to the micro:bit is to go on the Microsoft
+# MakeCode website and paste the code in and then flash from there https://makecode.microbit.org/
+
+# On Start - once again the smily face :D
+
+basic.show_leds("""
+    # . . . #
+    . . . . .
+    # # # # #
+    # . . . #
+    . # # # .
+    """)
+
+# The Forever Loop
+
+def on_forever():
+    if cuteBot.tracking(cuteBot.TrackingState.L_UNLINE_R_LINE): # when tracking sensors see black line on right side but not on left
+        cuteBot.motors(20, 10) # (left 20%, right 10% - turns right)
+    if cuteBot.tracking(cuteBot.TrackingState.L_LINE_R_UNLINE): # when tracking sensors see black line on left side but not on right
+        cuteBot.motors(10, 20) # (left 10%, right 20% - turns left)
+    if cuteBot.tracking(cuteBot.TrackingState.L_R_LINE): # black line on both sides
+        cuteBot.motors(20, 20) # moves forward at 20% speed
+basic.forever(on_forever)
